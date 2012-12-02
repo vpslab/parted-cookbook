e481d336322ca1454daac2432086bb5b222dc077
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-02 14:47:16 +0100

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "pawel.pacana@gmail.com"
 license          "All rights reserved"
 description      "Installs parted and provides resource for partitioning"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
